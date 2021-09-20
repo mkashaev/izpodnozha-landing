78fd7f8079752d44a8b8027aa50b55be272cff6a
Marat.Kashaev <marat.kashaev@x5.ru>
2021-09-20 10:39:40 +0300

    FIX meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Из под ножа`,
+    description: `Готовим и быстро привозим поке, супы, роллы и пиццу в которых правда много свежих море продуктов (и не только).`,
+    author: `@mkashaev`,
+    siteUrl: `https://izpodnozha.ru/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
